a5be8908170322c82385db4aa7b8b85df8260513
George Geeslin <george.geeslin@gmail.com>
2020-12-27 10:36:36 -0600

    setting up our react app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "lonestarresumes-1",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://alhfe96qx2.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_o09nD86Uh",
+      APP_CLIENT_ID: "6jstb5ulvhj4no88n9chkv1hb3",
+      IDENTITY_POOL_ID: "us-east-1:884904a8-9f16-4980-82fa-620d67a5bb55",
+    },
+  };
+  
+  export default config;
