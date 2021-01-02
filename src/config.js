1930071bd55a356b0466ec14c96c6f01d568302f
George Geeslin <george.geeslin@gmail.com>
2021-01-01 20:35:37 -0600

    building our react app

--- src/config.js
+++ src/config.js
@@ -13,6 +13,7 @@ const config = {
       APP_CLIENT_ID: "6jstb5ulvhj4no88n9chkv1hb3",
       IDENTITY_POOL_ID: "us-east-1:884904a8-9f16-4980-82fa-620d67a5bb55",
     },
+    STRIPE_KEY: "pk_test_51HqO1LBZZu9uF7XwmSzLpwuCcMUev9WQLl8yjyLMODegaJUSn1qugRv87euLZIAqiJ9Wj12E0CjTQkM6wKbR38y000Nniy9sU7",
   };
   
   export default config;
